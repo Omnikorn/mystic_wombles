2b4f31586315679c9c5f80443f253ec6887adfde
Harith Albadry <harith.albadry@gmail.com>
2023-05-17 17:21:23 +0100

    add text for all months of the year

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Grab values from the submitted form in the URL
-const words = new URLSearchParams(window.location.search);
+//const words = new URLSearchParams(window.location.search);
 
 // assign values to variable
 //const name= words.get("name")
@@ -15,13 +15,54 @@ function processForm (){
  
 let features =[]
 let fortune=[]
-switch month{
+switch (month){
     case jan:
         features =["big hearted", "self-motivate", "mature","calm","kind","natural leader","loyal","generous","adaptable","warm"]
         fortune=["You feel good about your self and people can tell", "People are attracted to you today and want to take part in you rhealing serenity","today you should listen without judgment as people pour their hearts to you","today you will fisht a lion and come out victorius","today you will feel like walking barefoot and playing with childre, its OK!"]
         break;
     case feb:
         features =["compassionate", "easy going", "empathetic","life of the party","kind","natural leader","loyal","original","strong","smart"]
-        fortune=["today you feel like its a great days in the country side", "People are attracted to you today and want to take part in you rhealing serenity","today you should 3 episodes of southpark","today you should give every doggie you see a treat","today is an excellent time to share your feelings with the person you love"]
+        fortune=["today you feel like its a great days in the country side", "People are attracted to you today and want to take part in you rhealing serenity","today you should watch 3 episodes of southpark","today you should give every doggie you see a treat","today is an excellent time to share your feelings with the person you love"]
+        break;
+        case mar:
+            features=["compassionate", "caring", "empathetic","artistic","kind","sensetive","loyal","organised","loving","creative"]
+            fortune=["today use your deep intuition to solve a lingering problem", "today you might struggle to wrap your head around a certain problem","today you are full of fuzzy energy","today you should give every doggie you see a treat","today you will be the reigning drama queen"]
+            break;
+        case apr:
+            features =["compassionate", "easy going", "empathetic","life of the party","kind","natural leader","loyal","original","strong","smart"]
+            fortune=["today you feel like its a great days in the country side", "People are attracted to you today and want to take part in you rhealing serenity","today you should watch 3 episodes of southpark","today you should give every doggie you see a treat","today is an excellent time to share your feelings with the person you love"]
+            break;
+        case may:
+            features =["big hearted", "self-motivate", "mature","calm","kind","natural leader","loyal","generous","adaptable","warm"]
+            fortune=["You feel good about your self and people can tell", "People are attracted to you today and want to take part in you rhealing serenity","today you should listen without judgment as people pour their hearts to you","today you will fisht a lion and come out victorius","today you will feel like walking barefoot and playing with childre, its OK!"]
+            break;
+        case jun:
+            features=["compassionate", "caring", "empathetic","artistic","kind","sensetive","loyal","organised","loving","creative"]
+            fortune=["today use your deep intuition to solve a lingering problem", "today you might struggle to wrap your head around a certain problem","today you are full of fuzzy energy","today you should give every doggie you see a treat","today you will be the reigning drama queen"]
+            break;
+        case jul:
+            features =["compassionate", "easy going", "empathetic","life of the party","kind","natural leader","loyal","original","strong","smart"]
+            fortune=["today you feel like its a great days in the country side", "People are attracted to you today and want to take part in you rhealing serenity","today you should watch 3 episodes of southpark","today you should give every doggie you see a treat","today is an excellent time to share your feelings with the person you love"]
+            break;
+        case aug:
+            features=["compassionate", "caring", "empathetic","artistic","kind","sensetive","loyal","organised","loving","creative"]
+            fortune=["today use your deep intuition to solve a lingering problem", "today you might struggle to wrap your head around a certain problem","today you are full of fuzzy energy","today you should give every doggie you see a treat","today you will be the reigning drama queen"]
+            break;
+            case sep:
+            features=["compassionate", "caring", "empathetic","artistic","kind","sensetive","loyal","organised","loving","creative"]
+            fortune=["today use your deep intuition to solve a lingering problem", "today you might struggle to wrap your head around a certain problem","today you are full of fuzzy energy","today you should give every doggie you see a treat","today you will be the reigning drama queen"]
+            break;
+        case oct:
+            features =["compassionate", "easy going", "empathetic","life of the party","kind","natural leader","loyal","original","strong","smart"]
+            fortune=["today you feel like its a great days in the country side", "People are attracted to you today and want to take part in you rhealing serenity","today you should watch 3 episodes of southpark","today you should give every doggie you see a treat","today is an excellent time to share your feelings with the person you love"]
+            break;
+        case nov:
+            features=["compassionate", "caring", "empathetic","artistic","kind","sensetive","loyal","organised","loving","creative"]
+            fortune=["today use your deep intuition to solve a lingering problem", "today you might struggle to wrap your head around a certain problem","today you are full of fuzzy energy","today you should give every doggie you see a treat","today you will be the reigning drama queen"]
+            break;
+            case dec:
+                features=["compassionate", "caring", "empathetic","artistic","kind","sensetive","loyal","organised","loving","creative"]
+                fortune=["today use your deep intuition to solve a lingering problem", "today you might struggle to wrap your head around a certain problem","today you are full of fuzzy energy","today you should give every doggie you see a treat","today you will be the reigning drama queen"]
+                break;
 }
 }
